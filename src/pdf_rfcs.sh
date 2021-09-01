b2dd89a245ba19079c97221c2054c50421353651
A. Feder Cooper <afc78@cornell.edu>
2021-09-01 13:47:20 -0400

    reorg, pdf missed rfcs

--- src/pdf_rfcs.sh
+++ src/pdf_rfcs.sh
@@ -2,7 +2,7 @@
 
 # first arg should be the file to read from
 # second arg should be the file path (no trailing /) to
-# find the files in
+# find the rfc files in
 # third arg should be the directory to cp the files to
 # (no trailing /)
 
